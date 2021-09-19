19d4ad4f8981b9c623b00788c752852e2dfdc693
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-19 11:23:05 +0800

    Create leetcode_70.py

---
+++ leetcode_70.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0]*(n+2)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-2]+dp[i-1]
+        return dp[n]
+
+S = Solution()
+print(S.climbStairs(1))
