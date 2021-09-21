84b83e03acf59c0df4b6af8f60555603fb189869
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-21 19:33:56 +0800

    Create leetcode_27.py

---
+++ leetcode_27.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        fast = 0
+        slow = 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+s = Solution()
+s.removeElement([0,1,2,2,3,0,4,2], 2)
