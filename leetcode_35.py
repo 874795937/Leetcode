e554a85d498590c34b7a7ed74e2039233c53842d
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-22 19:22:06 +0800

    Update leetcode_35.py

--- leetcode_35.py
+++ leetcode_35.py
@@ -17,11 +17,7 @@ class Solution(object):
             else:
                 return m
         return i
-        
+
 s = Solution()
 a = s.searchInsert([1,3,5,6,7],2)
 print(a)
-    # i = 1, mid = 3, j = 6, target = 2
-    # i = 1, mid = 1, j = 3, target = 2
-    #
-    # 1,2,3,4,5
