dcca9986293812d6338a241145f65e99f3860603
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-23 11:34:56 +0800

    Create leetcode_136.py

---
+++ leetcode_136.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set()
+        repeated = []
+        for item in nums:
+            if item not in seen:
+                seen.add(item)
+            else:
+                repeated.append(item)
+        # print(repeated)
+        for item in nums:
+            if item not in repeated:
+                return item
+# 把if else写全避免出问题
+class Solution1(object):
+    def singleNumber(self, nums):
+        nums = sorted(nums)
+        print(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                pre = nums[i]
+                # print("2333",i, pre)
+            else:
+                if nums[i] == pre:
+                    pre = -1
+                    # print(pre)
+                elif nums[i] != pre and pre == -1:
+                    print(pre, nums[i])
+                    pre = nums[i]
+                    # print(pre, nums[i])
+        return pre
+        
+s = Solution1()
+a = s.singleNumber([2,1,2])
+print(a)
