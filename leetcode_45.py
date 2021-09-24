16f2b12ec960822c116bdcaff88d7023171b1539
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-24 09:50:06 +0800

    Update leetcode_45.py

--- leetcode_45.py
+++ leetcode_45.py
@@ -9,16 +9,30 @@ class Solution(object):
         jump = 0
         while start < len(nums)-1:
             maxLength = 0
-            for i in range(nums[start]):
-                maxLength = max(maxLength, i+nums[start+1+i])
-                if i+nums[start+1+i] == maxLength:
-                    start += 1+i
-                if maxLength+start >= len(nums)-1:
-                    start += 1+i
-                    break
+            temp = 0
+            temp = nums[start]
+            if start+nums[start] < len(nums)-1:
+                for i in range(temp):
+                    maxLength = max(maxLength, 1+i+nums[start+i+1])
+                    if 1+i+nums[start+1+i] == maxLength:
+                        temp = start+1+i
+                start = temp
+
+            else:
+                start += nums[start]
             jump += 1
         return jump
 
+class Solution(object):
+    def jump(self, nums):
+        j = 0
+        dp = [0]*len(nums)
+        for i in range(1, len(nums)):
+            while nums[j]+j < i:
+                j += 1
+            dp[i] = dp[j] + 1
+        return dp[len(nums)-1]
+
 s = Solution()
-a = s.jump([2,3,1,1,3,5,3])
-print(a)
+a = s.jump([2,3,1])
+print(a,"ans")
