4f231bf3e3bd52ba22b974d14aeff3346f1f8f19
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-24 10:52:08 +0800

    Update leetcode_64.py

--- leetcode_64.py
+++ leetcode_64.py
@@ -7,6 +7,7 @@ class Solution(object):
         m = len(grid[0])
         n = len(grid)
         dp = [[0 for i in range(m)] for j in range(n)]
+        # road = []
         dp[0][0] = grid[0][0]
         # print(dp, m, n)
         for i in range(n):
@@ -17,6 +18,7 @@ class Solution(object):
                     dp[i][j] = dp[i][j-1]+grid[i][j]
                 if i > 0 and j == 0:
                     dp[i][j] = dp[i-1][j]+grid[i][j]
+                # road.append(grid[i][j])
         return dp[n-1][m-1]
 
 s = Solution()
