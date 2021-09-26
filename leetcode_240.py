2ad352c3920a4d5f04eb8a85b4986f5b31af9080
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-26 19:08:45 +0800

    Create leetcode_240.py

---
+++ leetcode_240.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rowNum = len(matrix)
+        columnNum = len(matrix[0])
+        i = 0
+        j = columnNum-1
+        while i <= rowNum-1 and j >= 0:
+            if matrix[i][j] > target:
+                j -= 1
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                return True
+        return False
