5a8e64297f3cf324f333404911ce080a1ae029ce
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-26 14:48:09 +0800

    Update leetcode_445.py

--- leetcode_445.py
+++ leetcode_445.py
@@ -11,3 +11,63 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        def reverse(pre, cur):
+            while cur:
+                next = cur.next
+                cur.next = pre
+                pre = cur
+                cur = next
+            return pre
+
+        head1 = reverse(None, l1)
+        head2 = reverse(None, l2)
+
+        add = 0
+        pre = None
+        while head1 or head2 or add:
+            if head1:
+                head1val = head1.val
+            else:
+                head1val = 0
+            if head2:
+                head2val = head2.val
+            else:
+                head2val = 0
+            head1Next = head1.next
+            head2Next = head2.next
+            node =ListNode(0)
+            node.next = pre
+            total = add+head1val+head2val
+            if total >= 10:
+                node.val = total%10
+                add = 1
+            else:
+                node.val = total
+                add = 0
+            pre = node
+            head1 = head1Next
+            head2 = head2Next
+        return pre
+
+# node1 = ListNode(7)
+node2 = ListNode(2)
+# node3 = ListNode(4)
+# node4 = ListNode(3)
+
+node5 = ListNode(5)
+# node6 = ListNode(6)
+# node7 = ListNode(4)
+
+# node1.next = node2
+# node2.next = node3
+# node3.next = node4
+
+# node5.next = node6
+# node6.next = node7
+
+s = Solution()
+head = s.addTwoNumbers(node2, node5)
+
+while head:
+    print(head.val)
+    head = head.next
