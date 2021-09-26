4c1366daf38041bfb9b450530d730d648eb9d8d8
874795937 <30828574+874795937@users.noreply.github.com>
2021-09-26 17:05:23 +0800

    Update leetcode_112.py

--- leetcode_112.py
+++ leetcode_112.py
@@ -5,6 +5,7 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
+# 涉及到叶节点的题都要判断一下
 class Solution(object):
     def hasPathSum(self, root, targetSum):
         """
@@ -14,34 +15,35 @@ class Solution(object):
         """
         def dfs(root, target):
             if not root:
+                return False
+
+            if not root.left and not root.right:
                 if target == 0:
-                    self.result = True
-                return
+                    return True
             target -= root.val
-            dfs(root.left, target)
-            dfs(root.right, target)
-        dfs(root, targetSum)
-        return self.result
+            return dfs(root.left, target) or dfs(root.right, target)
+        result = dfs(root, targetSum)
+        return result
 
-node1 = TreeNode(1)
-# node2 = TreeNode(2)
-# node3 = TreeNode(8)
-# node4 = TreeNode(11)
-# node5 = TreeNode(13)
-# node6 = TreeNode(4)
-# node7 = TreeNode(7)
-# node8 = TreeNode(2)
-# node9 = TreeNode(1)
+node1 = TreeNode(5)
+node2 = TreeNode(4)
+node3 = TreeNode(8)
+node4 = TreeNode(11)
+node5 = TreeNode(13)
+node6 = TreeNode(4)
+node7 = TreeNode(7)
+node8 = TreeNode(2)
+node9 = TreeNode(1)
 
-# node1.left = node2
-# node1.right = node3
-# node2.left = node4
-# node4.left = node7
-# node4.right = node8
-# node3.left = node5
-# node3.right = node6
-# node6.right = node9
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node4.left = node7
+node4.right = node8
+node3.left = node5
+node3.right = node6
+node6.right = node9
 
 s = Solution()
-a = s.hasPathSum(node1, 1)
+a = s.hasPathSum(node1, 22)
 print(a)
